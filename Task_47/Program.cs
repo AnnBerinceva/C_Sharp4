44b547d9bef509ed763041ba5a349105a9a170a8
AnnBerinceva <berinceva92@bk.ru>
2022-11-30 11:51:29 +0500

    task_47

---
+++ Task_47/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Количество строк ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов ");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            array[a, b]= Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        Console.Write("[ ");
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            Console.Write(array[a, b] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
